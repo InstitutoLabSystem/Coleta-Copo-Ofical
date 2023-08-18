3363356dda684f94a0be1509729243564d905c03
LabsystemDev1 <137182284+LabsystemDev1@users.noreply.github.com>
2023-08-18 13:28:55 -0300

    primeiro Deply

---
+++ Coleta-Copo-Oficial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coleta_Copo_Oficial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
